edc77285a1161fd6fdff9171386dead093606b6c
Moody Salem <moody.salem@gmail.com>
2018-02-22 02:35:07 -0500

    rename functino

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
     module: {
         loaders: [
             {test: /\.ts(x?)$/, loader: 'ts-loader'}
-        ]
+        ],
+        noParse: [/\/dtrace-provider\.js$/, /\/buffer-util\.js$/, /\/validation\.js$/]
     }
 };
