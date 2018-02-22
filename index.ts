99b15705b093d65713dd0e79a53dee948a75318f
Salem <salmahmo@amazon.com>
2018-02-22 11:49:42 -0500

    get wss client smarter

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ if (isNaN(msRuntime)) {
   msRuntime = 120000;
 }
 
-function getClient(): EthClient {
+async function getClient(): Promise<EthClient> {
   if (!NODE_URL) {
     throw new Error('missing node url');
   }
@@ -26,19 +26,35 @@ function getClient(): EthClient {
   } else if (NODE_URL.toLowerCase().startsWith('wss:/')) {
     const ws = new WebSocket(NODE_URL);
 
-    return new EthWsClient({ ws });
+    return new Promise<EthWsClient>((resolve, reject) => {
+      let opened = false;
+
+      ws.on('open', () => {
+        opened = true;
+        resolve(new EthWsClient({ ws }));
+      });
+
+      setTimeout(
+        () => {
+          if (!opened) {
+            reject(new Error('connection open timed out'));
+          }
+        },
+        5000
+      );
+    });
   } else {
-    throw new Error('unknown url protocol')
+    throw new Error('unknown url protocol');
   }
 }
 
 export const start: Handler = (event: any, context: Context, cb: Callback) => {
-  const client = getClient();
-
   const START_TIME = new Date().getTime();
   const runtime = () => (new Date().getTime()) - START_TIME;
 
   (async () => {
+    const client = await getClient();
+
     const clientVersion = await client.web3_clientVersion();
     parentLogger.info('client version', clientVersion);
 
