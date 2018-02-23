bfee0cb16c2d7f3689e73515865d838567ca6820
Moody Salem <moody.salem@gmail.com>
2018-02-23 01:29:20 -0500

    put block insertions on a queue

--- src/handle.ts
+++ src/handle.ts
@@ -1,12 +1,16 @@
 import { DynamoDBStreamHandler } from 'aws-lambda';
 import logger from './util/logger';
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDB, SQS } from 'aws-sdk';
 import { BlockWithTransactionHashes } from './util/model';
-import { DESTINATION_QUEUE_URL } from './util/env';
+import { SQS_BLOCK_RECEIVED_QUEUE_URL } from './util/env';
 
-export const start: DynamoDBStreamHandler = (event, context, cb) => {
+type BlockKey = Pick<BlockWithTransactionHashes, 'hash' | 'number'>
+
+const sqs = new SQS();
+
+export const start: DynamoDBStreamHandler = async (event, context, cb) => {
   // pull out the block keys
-  const blocks: Pick<BlockWithTransactionHashes, 'hash' | 'number'>[] = event.Records.map(
+  const blocks: BlockKey[] = event.Records.map(
     (record) =>
       record &&
       record.eventName === 'INSERT' &&
@@ -17,5 +21,24 @@ export const start: DynamoDBStreamHandler = (event, context, cb) => {
         ) : null
   ).filter(e => e !== null) as any;
 
-  logger.info({ blocks, destinationQueue: DESTINATION_QUEUE_URL }, 'received notification of block insertion');
+  logger.info({ blocks, destinationQueue: SQS_BLOCK_RECEIVED_QUEUE_URL }, 'received notification of block insertion');
+
+  for (let i = 0; i < blocks.length; i++) {
+    try {
+      await handleBlock(blocks[i]);
+    } catch (err) {
+      logger.error({ err }, 'failed to handle block');
+      context.done(err);
+    }
+  }
 };
+
+async function handleBlock(block: BlockKey): Promise<void> {
+  logger.info({ block }, 'enqueuing block');
+  await sqs.sendMessage({
+    MessageBody: JSON.stringify(block),
+    MessageDeduplicationId: block.hash,
+    MessageGroupId: '1',
+    QueueUrl: SQS_BLOCK_RECEIVED_QUEUE_URL
+  }).promise();
+}
--- src/util/env.ts
+++ src/util/env.ts
@@ -13,8 +13,8 @@ export const RUN_TIME_LENGTH_SECONDS: number = env.get('RUN_TIME_LENGTH_SECONDS'
 
 export const BLOCK_DATA_TTL_MS: number = env.get('BLOCK_DATA_TTL_MS').required().asIntPositive();
 
+// OWNED RESOURCES
 export const BLOCKS_TABLE: string = env.get('BLOCKS_TABLE').required().asString();
 export const LOGS_TABLE: string = env.get('LOGS_TABLE').required().asString();
 export const BLOCKSTREAM_STATE_TABLE: string = env.get('BLOCKSTREAM_STATE_TABLE').required().asString();
-
-export const DESTINATION_QUEUE_URL: string = env.get('DESTINATION_QUEUE_URL').required().asUrlString();
+export const SQS_BLOCK_RECEIVED_QUEUE_URL: string = env.get('SQS_BLOCK_RECEIVED_QUEUE_URL').required().asUrlString();
