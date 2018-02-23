0d86c50cc952cdb5953aa8b6d5bdcfbd197abe1d
Moody Salem <moody.salem@gmail.com>
2018-02-23 00:08:51 -0500

    fix logs env bug

--- src/util/env.ts
+++ src/util/env.ts
@@ -14,7 +14,7 @@ export const RUN_TIME_LENGTH_SECONDS: number = env.get('RUN_TIME_LENGTH_SECONDS'
 export const BLOCK_DATA_TTL_MS: number = env.get('BLOCK_DATA_TTL_MS', String(1000 * 86400 * 7)).asIntPositive();
 
 export const BLOCKS_TABLE: string = env.get('BLOCKS_TABLE').required().asString();
-export const LOGS_TABLE: string = env.get('BLOCKS_TABLE').required().asString();
+export const LOGS_TABLE: string = env.get('LOGS_TABLE').required().asString();
 export const BLOCKSTREAM_STATE_TABLE: string = env.get('BLOCKSTREAM_STATE_TABLE').required().asString();
 
 export const DESTINATION_QUEUE_URL: string = env.get('DESTINATION_QUEUE_URL').required().asUrlString();
