0576ab97e814b01cf9b2fab3adbac38e97796e49
Moody Salem <moody.salem@gmail.com>
2018-02-23 00:41:19 -0500

    put defaults in serverless

--- src/util/env.ts
+++ src/util/env.ts
@@ -4,14 +4,14 @@ import { LogLevel } from 'bunyan';
 export const SRC_NODE_URL: string = env.get('SRC_NODE_URL').required().asUrlString();
 export const NETWORK_ID: number = env.get('NETWORK_ID').required().asIntPositive();
 
-export const LOG_LEVEL: LogLevel = env.get('LOG_LEVEL', 'info').asString() as LogLevel;
+export const LOG_LEVEL: LogLevel = env.get('LOG_LEVEL').required().asString() as LogLevel;
 
-export const STARTING_BLOCK: number = env.get('STARTING_BLOCK', '5140211').asIntPositive();
-export const NUM_BLOCKS_PER_LOOP: number = env.get('NUM_BLOCKS_PER_LOOP', '1').asIntPositive();
+export const STARTING_BLOCK: number = env.get('STARTING_BLOCK').required().asIntPositive();
+export const NUM_BLOCKS_PER_LOOP: number = env.get('NUM_BLOCKS_PER_LOOP').required().asIntPositive();
 
-export const RUN_TIME_LENGTH_SECONDS: number = env.get('RUN_TIME_LENGTH_SECONDS', '180').asIntPositive();
+export const RUN_TIME_LENGTH_SECONDS: number = env.get('RUN_TIME_LENGTH_SECONDS').required().asIntPositive();
 
-export const BLOCK_DATA_TTL_MS: number = env.get('BLOCK_DATA_TTL_MS', String(1000 * 86400 * 7)).asIntPositive();
+export const BLOCK_DATA_TTL_MS: number = env.get('BLOCK_DATA_TTL_MS').required().asIntPositive();
 
 export const BLOCKS_TABLE: string = env.get('BLOCKS_TABLE').required().asString();
 export const LOGS_TABLE: string = env.get('LOGS_TABLE').required().asString();
