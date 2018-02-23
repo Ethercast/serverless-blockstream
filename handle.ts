eec22802cf62d73551fddbf77a6b70a3a2e07cbf
Moody Salem <moody.salem@gmail.com>
2018-02-22 21:25:12 -0500

    filter blocks

--- handle.ts
+++ handle.ts
@@ -4,8 +4,11 @@ import { DynamoDB } from 'aws-sdk';
 
 export const start: DynamoDBStreamHandler = (event, context, cb) => {
   const blocks = event.Records.map(
-    ({ dynamodb: { NewImage } }) => DynamoDB.Converter.marshall(NewImage)
-  );
+    (record) => record && record.dynamodb && record.dynamodb.NewImage ?
+      DynamoDB.Converter.marshall(
+        record.dynamodb.NewImage
+      ) : null
+  ).filter(e => e !== null);
 
   logger.info({ blocks }, 'handling blocks');
 };
