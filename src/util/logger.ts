4f7f4f8ae955fd7d130daf19a246e553a2a08a2c
Moody Salem <moody.salem@gmail.com>
2018-02-26 22:09:03 -0500

    also serialize state in metadata

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,19 +2,28 @@ import * as bunyan from 'bunyan';
 import { LOG_LEVEL } from './env';
 import _ = require('underscore');
 
+function stateSerializer(state) {
+  if (state && state.history && _.isArray(state.history)) {
+    return {
+      ..._.omit(state, 'history'),
+      historyLength: state.history.length
+    };
+  }
+  return state;
+}
+
 export default bunyan.createLogger({
   level: LOG_LEVEL,
   name: 'bunyan',
   serializers: {
     ...bunyan.stdSerializers,
-    state: function stateSerializer(state) {
-      if (state && state.history && _.isArray(state.history)) {
-        return {
-          ..._.omit(state, 'history'),
-          historyLength: state.history.length
-        };
+    state: stateSerializer,
+    metadata: function metadataSerializer(metadata) {
+      if (metadata && metadata.state) {
+        return { ...metadata, state: stateSerializer(metadata.state) };
       }
-      return state;
+
+      return metadata;
     }
   }
 });
