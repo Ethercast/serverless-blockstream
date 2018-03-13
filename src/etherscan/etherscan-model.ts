fe217081418157480b57508987a527533e6e699a
Moody Salem <moody.salem@gmail.com>
2018-03-12 20:29:26 -0400

    improve etherscan model

--- src/etherscan/etherscan-model.ts
+++ src/etherscan/etherscan-model.ts
@@ -1,4 +1,4 @@
-import { array, boolean, object, string } from 'joi';
+import * as Joi from 'joi';
 
 export interface EtherscanResponse {
   message: 'NOTOK' | 'OK';
@@ -17,11 +17,16 @@ export interface Output {
   type: string;
 }
 
+export interface Tuple extends Output {
+  type: 'tuple',
+  components: Output[]
+}
+
 interface ContractMember {
   constant?: boolean;
   inputs?: Input[];
   name?: string;
-  outputs?: Output[];
+  outputs?: (Output | Tuple)[];
   type: string;
   payable?: boolean;
   stateMutability?: string;
@@ -30,26 +35,33 @@ interface ContractMember {
 
 export type Abi = ContractMember[];
 
-export const JoiInput = object({
-  name: string().allow(''),
-  type: string(),
-  indexed: boolean()
+export const JoiInput = Joi.object({
+  name: Joi.string().allow(''),
+  type: Joi.string(),
+  indexed: Joi.boolean()
 });
 
-export const JoiOutput = object({
-  name: string().allow(''),
-  type: string()
+export const JoiOutput = Joi.object({
+  name: Joi.string().allow(''),
+  type: Joi.string()
 });
 
-export const JoiContractMember = object({
-  constant: boolean(),
-  inputs: array().items(JoiInput),
-  name: string().allow(''),
-  outputs: array().items(JoiOutput),
-  type: string(),
-  payable: boolean(),
-  stateMutability: string(),
-  anonymous: boolean()
+export const JoiTuple = JoiOutput.keys({
+  type: Joi.string().valid('tuple').required(),
+  components: Joi.array().items(JoiOutput).min(1).required()
 });
 
-export const JoiAbi = array().items(JoiContractMember);
+export const JoiContractMember = Joi.object({
+  constant: Joi.boolean(),
+  inputs: Joi.array().items(JoiInput),
+  name: Joi.string().allow(''),
+  outputs: Joi.array().items(
+    Joi.alternatives(JoiOutput, JoiTuple)
+  ),
+  type: Joi.string(),
+  payable: Joi.boolean(),
+  stateMutability: Joi.string(),
+  anonymous: Joi.boolean()
+});
+
+export const JoiAbi = Joi.array().items(JoiContractMember);
